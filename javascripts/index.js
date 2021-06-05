bcc7432128a5a4de11cdcc28a721171a9a0f14a9
Karson <kars.kalt@gmail.com>
2021-06-05 11:27:08 -0400

    Added cellArray function.

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,1 +1,25 @@
+const cellArray = () => {
+    const array = []
+    const addNumber = array =>{
+        let i = 1
+        while (i <= 64) {
+            array.push(i)
+            i++
+        }
+    }
+    addNumber(array)
+    return array
+}
+
 const findCell = num => document.getElementById(`${num}`)
+
+
+const switchToBlack = element => {
+    element.classList.add("black")
+}
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    // document.addEventListener("click", )
+})
