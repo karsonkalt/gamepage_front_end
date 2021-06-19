de6551300f6e6327806c8f8d622f02346e30f05d
Karson <kars.kalt@gmail.com>
2021-06-19 15:13:47 -0400

    Renamed Score OO files.

--- javascripts/components/score.js
+++ javascripts/components/score.js
@@ -1,28 +1,38 @@
-const addAllUserScoreDivs = json => {
-    if (userScoresContainer() != null) {
-        userScoresContainer().remove()
-    } else {
-        if (topUserContainer() != null) {
-            topUserContainer().remove()
+class Score {
+    static addAllUserScoreDivs(json) {
+        if (userScoresContainer() != null) {
+            userScoresContainer().remove()
+        } else {
+            if (topUserContainer() != null) {
+                topUserContainer().remove()
+            }
+            const div = document.createElement("div")
+            div.id = "userScoresContainer"
+            const parent = playSpace()
+            parent.appendChild(div)
+
+            Object.entries(json).forEach(scoreObject => {
+                Score.addUserScore(scoreObject[1])
+            })
         }
-        const div = document.createElement("div")
-        div.id = "userScoresContainer"
-        const parent = playSpace()
-        parent.appendChild(div)
-
-        Object.entries(json).forEach(scoreObject => {
-            addUserScore(scoreObject[1])
-        })
     }
-}
 
-const addUserScore = scoreObject => {
-    const div = document.createElement("div")
-    div.classList.add("userScore")
-    div.innerHTML = `
-        ${scoreObject.created_at}
-        <div class="gameScore">Score: ${scoreObject.points}</div>
-    `
-    const parent = userScoresContainer()
-    parent.appendChild(div)
+    static addUserScore(score) {
+        const div = document.createElement("div")
+        div.classList.add("userScore")
+        div.innerHTML = `
+            ${score.created_at}
+            <div class="gameScore">Score: ${score.points}</div>
+        `
+        const parent = userScoresContainer()
+        parent.appendChild(div)
+    }
+    
+
+    // Instance Methods
+    constructor(points, createdAt) {
+        this.points = points
+        this.createdAt = createdAt
+    }
+
 }
--- javascripts/services/scoreAPI.js
+++ javascripts/services/scoreAPI.js
@@ -1,26 +1,25 @@
-const sendScoreToServer = (winnerUsername, score) => {
-    fetch(root + "/scores/", {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*'
-        },
-        body: JSON.stringify({username: winnerUsername, score: score})
-    })
-    .then(resp => resp.json())
-    .then(json => {
-        // Do something here to show the user.
-        console.log(json)
-    })
-}
+class ScoreAPI {
+    static sendScoreToServer (winnerUsername, score) {
+        fetch(root + "/scores/", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+            body: JSON.stringify({username: winnerUsername, score: score})
+        })
+            .then(resp => resp.json())
+            .then(json => {
+                // Do something here to show the user.
+                console.log(json)
+            })
+    }
 
-const getCurrentUserScores = () => {
-    fetch(root + "/users/" + currentUserId + "/scores")
-        .then(resp => resp.json())
-        .then(json => {
-            // Do something here to show the user.
-            // console.log(json)
-            addAllUserScoreDivs(json)
-        }) 
-
+    static getCurrentUserScores() {
+        fetch(root + "/users/" + currentUserId + "/scores")
+            .then(resp => resp.json())
+            .then(json => {
+                Score.addAllUserScoreDivs(json)
+            }) 
+    }
 }
