6be528157baadfdb814cc2f3c47c43377a8b380f
Karson <kars.kalt@gmail.com>
2021-06-19 14:31:31 -0400

    Reordered index.html script.

--- javascripts/global.js
+++ javascripts/global.js
@@ -17,3 +17,28 @@ const header = () => document.querySelector("#header")
 const logoutButton = () => document.querySelector("#logout")
 const topUserContainer = () => document.querySelector("#topUserContainer")
 
+// From old index file. Where does this belong?
+
+const root = "http://localhost:3000"
+
+let currentUsername = ""
+let currentUserId = ""
+
+let opponentUsername = ""
+let opponentUserId = ""
+
+const userNames = () => {
+    return [currentUsername, opponentUsername]
+}
+
+let currentUserUsername = () => {
+    if (numberOfTurns() % 2 == 0 ) {
+        return userNames()[0]
+    } else {
+        return userNames()[1]
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    addLoginDiv()
+})
